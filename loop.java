c65f0f4a0869f3f638cfad88eea27d8f547a16c9
Bibhav kumar <bibhavkumar05@gmail.com>
2025-05-23 16:50:58 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class loop {
+    public static void main(String[] args) {
+     // For loop   
+        // for(int i=0; i<10; i++){
+        //     System.out.println("Hello World");
+        // }
+     //While loop   
+        // int i= 0;
+        // while(i<5){
+        //     System.out.println("Hello World");
+        //     i++;
+ 
+        // }
+    // Do While loop 
+        // int i =0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+
+        // }
+        // while(i<11);
+// To add first n natural number 
+        // Scanner sc =new Scanner(System.in);
+        // System.out.println("Enter the maximum value of natural number:  ");
+        // int n =sc.nextInt();
+
+        // int sum =0;
+        // for(int i=0; i<=n;i++){
+        //     sum=i+sum;
+        // }
+        // System.out.println("Sum of the value upto" + " "+ n +" "+ "natural number: " +sum);
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number that You need of multiplication table:  ");
+        int n =sc.nextInt();
+
+        for(int i=1; i<11; i++){
+            System.out.println(n*i);
+        }
+    }
+}
