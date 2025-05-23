23061f9986e1e7339a75c733785cbaac40a5e719
Bibhav kumar <bibhavkumar05@gmail.com>
2025-05-23 15:21:04 +0530

    Add files via upload

---
+++ Condition.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Condition {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+//     System.out.println("Enter the age");
+//     int age =sc.nextInt();
+
+
+//     if(age>18){
+//         System.out.println("Adult");
+//     }
+//     else{System.err.println("Not Adult");  
+// }
+//     System.out.println("Enter the num: ");   
+//     int num= sc.nextInt();
+
+
+//     if(num%2==0){
+//         System.out.println("The given num is even");
+//     }
+//     else{
+//         System.out.println("The given number is odd");
+//     }
+
+    // System.out.println("Number: ");
+    // int Number= sc.nextInt();
+
+    // if(Number%2==0)
+    //     System.out.println("Bazinga");
+    System.out.println("Enter the button value: ");
+    int button= sc.nextInt();
+    switch(button){
+        case 1 : System.out.println("hello");
+        break;
+        case 2: System.out.println("namaste");
+        break;
+        case 3: System.out.println("Bonjour");
+        break;
+        default: System.out.println("invalid Buttom");
+
+
+
+    }
+    
+    
+    
+    }
+    }
+
+    
+
+
+    
+    
+   
+
