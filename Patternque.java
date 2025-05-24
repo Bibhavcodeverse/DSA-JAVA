8001dc53eec98299cecf5dedd3dea473028b16bc
Bibhav kumar <bibhavkumar05@gmail.com>
2025-05-24 06:55:15 +0530

    Add files via upload

---
+++ Patternque.java
@@ -0,0 +1,47 @@
+public class Patternque {
+    public static void main(String[] args) {
+            // for(int i=1;i<=5;i++){
+    //     for(int j=1;j<=5;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+
+    //Hollow rectangle
+//         int n=4;
+//         int m=5;
+// // outer loop 
+//     for(int i=1; i<=n; i++){
+//         // inner loop
+//         for(int j=1;j<=m; j++){
+//             if(i==1 || j==1 || i==n || j==m){
+//                 System.out.print("*");
+//             }
+
+//             else {
+//                 System.out.print(" ");
+//             }
+            
+//         }
+//         System.out.println();
+//          }
+
+
+
+
+// Half paramid 
+        int n=4;
+
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+
