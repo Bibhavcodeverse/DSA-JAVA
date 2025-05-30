b79c653ba130cb4281e376489e10656d42cca64f
Bibhav kumar <bibhavkumar05@gmail.com>
2025-05-30 18:06:04 +0530

    Update Patternque.java

--- Patternque.java
+++ Patternque.java
@@ -83,6 +83,42 @@ public class Patternque {
 
         }
 
+
+
+        // Floyd's triangle
+        
+
+        int n = 5;  // total number of rows 
+        int number=1;
+        for (int i=0; i<=n;i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(number+" ");
+                number++; // number =number +1;
+            }  
+            
+            System.out.println();
+          }
+          
+          // 0-1 traingle
+
+          int n=5;
+          for(int i=0;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                int sum=i+j;
+
+                if(sum%2==0){
+                    System.out.print("1");
+                
+                }
+
+                else{
+                    System.out.print("0");
+
+                }
+            }
+            System.out.println();
+          }
+
     }
 }
 
