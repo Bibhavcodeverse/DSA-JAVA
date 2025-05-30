3cd2aeb7e65bb3efa134d1cb434a491388ea9049
Bibhav kumar <bibhavkumar05@gmail.com>
2025-05-30 16:30:43 +0530

    Update Patternque.java

--- Patternque.java
+++ Patternque.java
@@ -72,6 +72,17 @@ public class Patternque {
         System.out.println();
          }
 
+         // inverted half paramid with numbers 
+
+        for (int i=n; i>=1; i--){
+            for (int j=1; j<=i;j++){
+                System.out.print(j);
+
+            }
+            System.out.println();
+
+        }
+
     }
 }
 
