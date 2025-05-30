6c50cddea6ab05e9d2902422595533457c8014d7
Bibhav kumar <bibhavkumar05@gmail.com>
2025-05-30 16:17:33 +0530

    Update Patternque.java

--- Patternque.java
+++ Patternque.java
@@ -1,31 +1,31 @@
 public class Patternque {
     public static void main(String[] args) {
-            // for(int i=1;i<=5;i++){
-    //     for(int j=1;j<=5;j++){
-    //         System.out.print("*");
-    //     }
-    //     System.out.println();
-    // }
+            for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 
 
-    //Hollow rectangle
-//         int n=4;
-//         int m=5;
-// // outer loop 
-//     for(int i=1; i<=n; i++){
-//         // inner loop
-//         for(int j=1;j<=m; j++){
-//             if(i==1 || j==1 || i==n || j==m){
-//                 System.out.print("*");
-//             }
+    Hollow rectangle
+        int n=4;
+        int m=5;
+// outer loop 
+    for(int i=1; i<=n; i++){
+        // inner loop
+        for(int j=1;j<=m; j++){
+            if(i==1 || j==1 || i==n || j==m){
+                System.out.print("*");
+            }
 
-//             else {
-//                 System.out.print(" ");
-//             }
+            else {
+                System.out.print(" ");
+            }
             
-//         }
-//         System.out.println();
-//          }
+         }
+         System.out.println();
+          }
 
 
 
@@ -39,6 +39,38 @@ public class Patternque {
             }
             System.out.println();
         }
+
+         int n =5;
+        for(int i=n; i>=1;i--){
+            for(int j=1; j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+        // inverted half paramid
+            // outer loop 
+           for(int i=1; i<=n;i++){
+            // inner loop-> space print
+            for(int j=1;j<=n-i; j++){
+                System.out.print(" ");
+            }
+            // inner loop -> star print
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+           }
+
+        // half paramid  with Numbers 
+
+         for (int i=1; i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+ " ");
+            }
+        System.out.println();
+         }
 
     }
 }
