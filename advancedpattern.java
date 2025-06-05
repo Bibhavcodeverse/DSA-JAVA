fb96e8397d9123a0c12c3515694adfb6775d7756
Bibhav kumar <bibhavkumar05@gmail.com>
2025-06-05 16:13:21 +0530

    Add files via upload

--- advancedpattern.java
+++ advancedpattern.java
@@ -2,97 +2,160 @@ public class advancedpattern {
     public static void main(String[] args) {
         // Butterfly pattern 
 
-        int n=5;
+        //int n=5;
 
         //First half--- upper half
 
 
-        for(int i=1; i<=n;i++){
-            // 1st part
+        // for(int i=1; i<=n;i++){
+        //     // 1st part
 
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
 
-            }
+        //     }
 
-        //space 
+        // //space 
 
-        int spaces= 2*(n-i);
-        for(int j=1; j<=spaces;j++){
-            System.out.print(" ");
+        // int spaces= 2*(n-i);
+        // for(int j=1; j<=spaces;j++){
+        //     System.out.print(" ");
 
-        }
+        // }
 
-        //2nd part 
+        // //2nd part 
 
-        for (int j=1; j<=i;j++){
-            System.out.print("*");
-        }
+        // for (int j=1; j<=i;j++){
+        //     System.out.print("*");
+        // }
 
-        System.out.println();
-        }
+        // System.out.println();
+        // }
 
-        // lower half
+        // // lower half
 
-        for(int i=n; i>=1;i--){
-            // 1st part
+        // for(int i=n; i>=1;i--){
+        //     // 1st part
 
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("*");
 
-            }
+        //     }
 
-        //space 
+        // //space 
 
-        int spaces= 2*(n-i);
-        for(int j=1; j<=spaces;j++){
-            System.out.print(" ");
+        // int spaces= 2*(n-i);
+        // for(int j=1; j<=spaces;j++){
+        //     System.out.print(" ");
 
-        }
+        // }
 
-        //2nd part 
+        // //2nd part 
 
-        for (int j=1; j<=i;j++){
-            System.out.print("*");
-        }
+        // for (int j=1; j<=i;j++){
+        //     System.out.print("*");
+        // }
 
-        System.out.println();
-        }
+        // System.out.println();
+        // }
 
 
 
-       // Solid Rhombus
+    
+    //}
 
-    int n=5;
-    for(int i=1;i<=n;i++){
-        // spaces 
-        for(int j=1; j<=n-i;j++){
-            System.out.print(" ");
+    //  Solid Rhombus
 
-        }
+    // int n=5;
+    // for(int i=1;i<=n;i++){
+    //     // spaces 
+    //     for(int j=1; j<=n-i;j++){
+    //         System.out.print(" ");
 
-        for(int j=1; j<=5;j++){
-            System.out.print("*");
-        }
+    //     }
 
-        System.out.println();
+    //     for(int j=1; j<=5;j++){
+    //         System.out.print("*");
+    //     }
 
-    }
+    //     System.out.println();
+
+    // }
 
         // number pyaramid  
-    int n=5;
-    for(int i=1; i<=n;i++){
-        // spaces
-        for(int j=1;j<=n-i; j++){
+    // int n=5;
+    // for(int i=1; i<=n;i++){
+    //     // spaces
+    //     for(int j=1;j<=n-i; j++){
+    //         System.out.print(" ");
+
+    //     }
+    //     // number
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print(i+" ");
+    //     }
+    //     System.out.println();
+    // }
+
+    // palindromic pattern
+
+     int n=5;
+     for(int i=1; i<=n;i++){
+        //space
+        for(int j=1;j<=n-1;j++){
             System.out.print(" ");
 
         }
-        // number
+
+        //1st half numbers
+
+        for(int j=i;j>=1;j--){
+            System.out.print(j);
+        }
+
+        //2nd half
         for(int j=1;j<=i;j++){
-            System.out.print(i+" ");
+            System.out.print(j);
+
         }
         System.out.println();
-    }
+     }
 
-    }
+     // Diamond pattern
+    //    int n = 4;
+
+    //     // Upper half of the diamond
+    //     for (int i = 1; i <= n; i++) {
+    //         // Print spaces
+    //         for (int j = 1; j <= n - i; j++) {
+    //             System.out.print(" ");
+    //         }
+
+    //         // Print stars
+    //         for (int j = 1; j <= 2 * i - 1; j++) {
+    //             System.out.print("*");
+    //         }
+
+    //         // Move to next line
+    //         System.out.println();
+    //     }
+
+    //     // Lower half of the diamond
+    //     for (int i = n - 1; i >= 1; i--) {
+    //         // Print spaces
+    //         for (int j = 1; j <= n - i; j++) {
+    //             System.out.print(" ");
+    //         }
+
+    //         // Print stars
+    //         for (int j = 1; j <= 2 * i - 1; j++) {
+    //             System.out.print("*");
+    //         }
+
+    //         // Move to next line
+    //         System.out.println();
+    //     }
 }
+
+}
+    
